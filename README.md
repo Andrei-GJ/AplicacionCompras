7e9c3c28f27d283ef742941951948608574e2064
Andrei-GJ <andreisotelo369@gmail.com>
2025-06-15 17:42:46 -0500

    Refactor input handling and calculations for client purchases in index.py

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # AplicacionCompras
 ## proyecto hecho con los siguientes requerimientos
 
-- tener la posibilidad de que se pueda aplicar la compra para N clientes(*)
+[x] tener la posibilidad de que se pueda aplicar la compra para N clientes(*)  
     se entiende como que se pueden tener multiples ventas
 
-- que cada cliente pueda llevar N cantidad de productos diferentes
+[x] que cada cliente pueda llevar N cantidad de productos diferentes
     que se pueda comprar un producto mas de una vez
 
-- que se calcule el total de compra por cliente
+[x] que se calcule el total de compra por cliente
 
-- que se calcule el total de compra de todos los clientes
+[] que se calcule el total de compra de todos los clientes
 
-- que se pueda saber cual fue el total de  compra mas alta dentro de todos los clientes que compraron.<!--  -->
+[] que se pueda saber cual fue el total de  compra mas alta dentro de todos los clientes que compraron.<!--  -->
--- index.py
+++ index.py
@@ -1,21 +1,29 @@
 nombresclientes = []
 listclient = {}
-print("Compras")
+valueFinalTotal = 0
 cantidad_clientes = int(input("Ingrese la cantidad de clientes: "))
-
-
+print("--" * 20)
 for i in range(cantidad_clientes):
     listclient[i] = {}
-    print("Cliente", i + 1)
-    
     nombre = input("Ingrese el nombre del cliente: ")
     print("Un gusto atenderlo ", nombre)
     nombresclientes.append(nombre)
-    
+    print("--" * 20)
     cantidad = int(input("Cuantas productos escogio el cliente: "))
     for a in range (cantidad):
         nameProducto = input("como se llama el producto: ")
         valueProduct = int(input("Ingrese el valor del producto: "))
-        listclient[i][nameProducto] = valueProduct
-    print(listclient[i])
-    print(listclient)
+        amountProduct = int(input("Ingrese la cantidad del producto: "))
+        valueTotal = valueProduct * amountProduct
+        print("El valor total del producto es: ", valueTotal)
+        listclient[i][nameProducto] = valueTotal  
+        print("tu cuenta va por un valor de: " + str(sum(listclient[i].values())))
+    print("--" * 20)
+for a in listclient:
+    # print("dsadjsa")
+    valueFinal = sum(listclient[a].values())
+    # print("dsadjsa")
+# print("dsadjsa")
+valueFinalTotal =+ valueFinal
+# print("dsadjsa")
+print(valueFinalTotal)
